116d24fd6d2e5bd340ea98f573d4b84504efa1c1
lordeko <mattia.ruggeri88@gmail.com>
2016-01-14 20:38:01 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,10 +1,15 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOTANIMATION_HALF_RES := false
+
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit some common AOKP stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Enhanced NFC
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 PRODUCT_DEVICE := hlte
-PRODUCT_NAME := cm_hlte
+PRODUCT_NAME := aokp_hlte

19bc7256ecd934369a7d2f2990b9c2df5879340a
lordeko <mattia.ruggeri88@gmail.com>
2016-01-17 08:44:13 +0100

    Update pac.mk

--- pac.mk
+++ pac.mk
@@ -1,10 +1,13 @@
+# Resolution values for bootanimation
+PAC_BOOTANIMATION_NAME := 1080
+
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Include PAC common configuration
+include vendor/pac/config/common_full_phone.mk
 
 # Enhanced NFC
 $(call inherit-product, vendor/pac/config/nfc_enhanced.mk)
-
-# Inherit some common stuff.
-$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := pac_hlte

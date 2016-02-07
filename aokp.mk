af03a2667bd6f65a2ba914c86dd7109a331317e2
lordeko <mattia.ruggeri88@gmail.com>
2016-02-07 12:52:47 +0100

    Update aokp.mk

--- aokp.mk
+++ aokp.mk
@@ -5,7 +5,7 @@ TARGET_BOOTANIMATION_HALF_RES := false
 
 $(call inherit-product, device/samsung/hlte/full_hlte.mk)
 
-# Inherit some common AOKP stuff.
+# Inherit some common CM stuff.
 $(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Enhanced NFC
@@ -13,3 +13,6 @@ $(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 PRODUCT_DEVICE := hlte
 PRODUCT_NAME := aokp_hlte
+
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_1080.zip:system/media/bootanimation.zip
